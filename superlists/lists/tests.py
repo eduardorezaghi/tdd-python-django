654d47b21df4218d028b69099cd952f63a8a3650
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-07-10 16:55:13 -0300

    Add Item Model (w/ Migration) alongside Unit Test

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,6 +3,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -15,3 +16,18 @@ class HomePageTest(TestCase):
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+
+        self.assertEqual(saved_items.count(), 2)
+        self.assertEqual(saved_items[0].text, "The first (ever) list item")
+        self.assertEqual(saved_items[1].text, "Item the second")
