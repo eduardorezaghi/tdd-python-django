6c00965fa2c755f37f2de88358181edca201c87f
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-07-10 17:38:51 -0300

    Refactor view to handle new item POSTs
    - Add a test for the new POST behavior

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -8,13 +8,22 @@ from lists.models import Item
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        response = self.client.post("/", data={"item_text": "A new list item"})
+
+        self.assertEqual(Item.objects.count(), 1)
+        self.assertEqual(Item.objects.first().text, "A new list item")
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["location"], "/")
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
