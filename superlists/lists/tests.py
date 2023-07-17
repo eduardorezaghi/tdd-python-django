ffb5addbddb7849f23e05c00e4bf6128349997e5
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-07-16 23:13:46 -0300

    Refactor view to use new URL /lists/new

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,4 +1,3 @@
-from urllib import response
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
@@ -11,21 +10,6 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
-
-    def test_can_save_a_POST_request(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
-
-        self.assertEqual(Item.objects.count(), 1)
-        self.assertEqual(Item.objects.first().text, "A new list item")
-
-    def test_redirects_after_POST(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response["location"], "/lists/the-only-list-in-the-world/")
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get("/")
-        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
@@ -58,3 +42,17 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, "item 1")
         self.assertContains(response, "item 2")
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post("/lists/new", data={"item_text": "A new list item"})
+
+        self.assertEqual(Item.objects.count(), 1)
+        self.assertEqual(Item.objects.first().text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            "/lists/new", data={"item_text": "A new list item"}
+        )
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
