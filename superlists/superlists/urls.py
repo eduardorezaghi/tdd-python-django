c34135b5cad2f1769c81cbfe913ac4823db3b360
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-07-18 22:56:04 -0300

    Use re_path() instead of path() in superlists/urls.py
    - Use RegexURLPattern instead of string

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import re_path
 from lists import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home_page, name="home"),
-    path('lists/new', views.new_list, name="new_list"),
-    path('lists/the-only-list-in-the-world/', views.view_list, name="view_list"),
+    re_path(r'^admin/$', admin.site.urls),
+    re_path(r'^$', views.home_page, name="home"),
+    re_path(r'^lists/new$', views.new_list, name="new_list"),
+    re_path(r'^lists/the-only-list-in-the-world/$', views.view_list, name="view_list"),
 ]
