371f4f0d4aa75f41bf6b377457d37d9204273399
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-07-18 22:32:05 -0300

    Add List and Item tests, models, and migrations for them
    - Change the List model to be able to have multiple items
    - Add a foreign key to Item to link it to a List
    - Change view to handle saving items to a list

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
+
 class Item(models.Model):
-    """Model definition for Item.
-    """
+    """Model definition for Item."""
+
     text = models.TextField(default="")
+    list = models.ForeignKey(
+        "List", default=None, null=True, on_delete=models.CASCADE
+    )
+
+
+class List(models.Model):
+    """Model definition for List."""
+    pass
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,5 @@
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 from django.test import TestCase
-from django.urls import resolve
-from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -12,21 +8,30 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, "home.html")
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "Item the second"
+        second_item.list = list_
         second_item.save()
 
-        saved_items = Item.objects.all()
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
+        saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
         self.assertEqual(saved_items[0].text, "The first (ever) list item")
         self.assertEqual(saved_items[1].text, "Item the second")
+        self.assertEqual(saved_items[0].list, list_)
+        self.assertEqual(saved_items[1].list, list_)
 
 
 class ListViewTest(TestCase):
@@ -35,6 +40,7 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, "list.html")
 
     def test_display_all_items(self):
+        list_ = List.objects.create()
         Item.objects.create(text="item 1")
         Item.objects.create(text="item 2")
 
