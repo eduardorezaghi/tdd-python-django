657d61d4aea710698ef1f1f7ec94e2c6dfd000ca
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-07-19 23:20:33 -0300

    Add urls.py for lists app
    - Refactor urls.py to use path() method (standard in Django)
    - Add lists/urls.py to handle lists app urls

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -90,6 +90,24 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
